cf0c5549cbb1604b93e53c5fbfde4e9ded4237e7
Chandani_50 <Chandani_50@192.168.1.3>
2018-05-13 09:49:04 +0530

    More test added

---
+++ SeleniumFramework/src/test/java/pages/ContactPage.java
@@ -0,0 +1,86 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import lib.Utility;
+
+public class ContactPage {
+
+	WebDriver driver;
+	String usernameValidation;
+
+	public ContactPage(WebDriver ldriver) {
+
+		this.driver = ldriver;
+	}
+	
+	@FindBy(how=How.XPATH,using="//*[text()='Admin']") 
+	WebElement adminTab;
+	
+	@FindBy(id="btnAdd") WebElement addButton;
+	
+	@FindBy(id="systemUser_userType") WebElement userType;
+	
+	@FindBy(xpath="(//input[contains(@id,'employeeName')])[1]") WebElement empName;
+	
+	@FindBy(xpath="//input[contains(@id,'userName')]") WebElement username;
+	
+	@FindBy(xpath="//input[contains(@id,'password')]") WebElement password;
+	
+	@FindBy(xpath="//input[contains(@id,'confirmPassword')]") WebElement confPass;
+	
+	@FindBy(xpath="//input[contains(@value,'Save')]") WebElement saveButton;
+		
+	String xpath="//*[contains(text(),'"+usernameValidation+"')]";
+
+	By validateUser=By.xpath(xpath);
+	
+	
+	public void navigateToContactPage()
+	{
+		
+		Utility.waitForWebElement(driver, adminTab, 20).click();;
+		
+		Utility.waitForWebElement(driver, addButton, 20).click();
+	}
+	
+	public void addContact(String unameForContact,String passforContact)
+	{
+		Select type=new Select(userType);
+		
+		type.selectByIndex(0);
+		
+		empName.sendKeys("Robert Craig");
+		
+		username.sendKeys(unameForContact);
+		
+		password.sendKeys(passforContact);
+		
+		confPass.sendKeys(passforContact);
+		
+		saveButton.click();
+		
+		usernameValidation=unameForContact;
+	
+	}
+	
+	public void validateContactAdded()
+	{
+		
+		WebDriverWait wait=new WebDriverWait(driver, 10);
+		
+		boolean status=wait.until(ExpectedConditions.presenceOfElementLocated(validateUser)).isDisplayed();
+		
+		Assert.assertTrue(status);
+		
+	}
+
+}
---
+++ SeleniumFramework/src/test/java/testcases/AddContacts.java
@@ -0,0 +1,53 @@
+package testcases;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import pages.BaseClass;
+import pages.ContactPage;
+import pages.LoginPage;
+import pages.LogoutPage;
+
+public class AddContacts extends BaseClass{
+
+	
+	@Parameters({"uname","pass"})
+	@Test
+	public void addContactAndValidate(String uname,String pass)
+	{
+	
+	logger=report.startTest("Add Contact in CRM", "This test case will add contacts in CRM");
+		
+	LoginPage login=PageFactory.initElements(driver, LoginPage.class);
+	
+	ContactPage contact=PageFactory.initElements(driver, ContactPage.class);
+	
+	LogoutPage logOut=PageFactory.initElements(driver, LogoutPage.class);
+	
+	login.loginToCRM(uname, pass);
+	
+	logger.log(LogStatus.INFO, "Login Done");
+	
+	contact.navigateToContactPage();
+	
+	logger.log(LogStatus.INFO, "landed to contact page");
+	
+	contact.addContact("Kabir500", "Kabir500");
+	
+	logger.log(LogStatus.INFO, "Contact added");
+	
+	contact.validateContactAdded();
+	
+	logger.log(LogStatus.PASS, "user validated");
+	
+	logOut.logOutFromApplication();
+	
+	logger.log(LogStatus.INFO, "User is logged off");
+	
+	}
+	
+	
+}
